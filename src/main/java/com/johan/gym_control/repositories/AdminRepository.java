a68002931fd54f118feb39e95ddc377e43370b39
Johan <johanvilla64@gmail.com>
2025-02-19 10:28:24 -0500

    refactor(repositories): :recycle: rename method findByUserEmail to findByAdminEmail in AdminRepository to search by the right attribute

--- src/main/java/com/johan/gym_control/repositories/AdminRepository.java
+++ src/main/java/com/johan/gym_control/repositories/AdminRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findByUserEmail(String email);
+  Optional<Admin> findByAdminEmail(String email);
 }
