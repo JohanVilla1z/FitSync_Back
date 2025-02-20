b1936a174e0e42074b017f19187d30a1a87dbed6
Johan <johanvilla64@gmail.com>
2025-02-20 17:35:18 -0500

    feat(services): :sparkles: integrate IMCTrackingObserver in UpdateUserCommand to notify on user updates

--- src/main/java/com/johan/gym_control/services/user/UpdateUserCommand.java
+++ src/main/java/com/johan/gym_control/services/user/UpdateUserCommand.java
@@ -1,19 +1,21 @@
 package com.johan.gym_control.services.user;
 
+import java.util.Optional;
+
 import com.johan.gym_control.models.User;
 import com.johan.gym_control.repositories.UserRepository;
 import com.johan.gym_control.services.imcTracking.UpdateIMCCommand;
 import com.johan.gym_control.services.interfaces.ICommandParametrized;
-
-import java.util.Optional;
+import com.johan.gym_control.services.observers.IMCTrackingObserver;
 
 public class UpdateUserCommand implements ICommandParametrized<Void, User> {
   private final UserRepository userRepository;
-  private final UpdateIMCCommand updateIMCCommand;
+  private final IMCTrackingObserver imcTrackingObserver;
 
-  public UpdateUserCommand(UserRepository userRepository, UpdateIMCCommand updateIMCCommand) {
+  public UpdateUserCommand(UserRepository userRepository, UpdateIMCCommand updateIMCCommand,
+      IMCTrackingObserver imcTrackingObserver) {
     this.userRepository = userRepository;
-    this.updateIMCCommand = updateIMCCommand;
+    this.imcTrackingObserver = imcTrackingObserver;
   }
 
   @Override
@@ -21,12 +23,12 @@ public class UpdateUserCommand implements ICommandParametrized<Void, User> {
     Optional<User> userOpt = userRepository.findById(user.getUserId());
     if (userOpt.isPresent()) {
       User existingUser = userOpt.get();
+      existingUser.addObserver(imcTrackingObserver);
       existingUser.setUserName(user.getUserName());
       existingUser.setUserLastName(user.getUserLastName());
       existingUser.setUserPhone(user.getUserPhone());
       existingUser.setUserWeight(user.getUserWeight());
       existingUser.setUserHeight(user.getUserHeight());
-      updateIMCCommand.execute(existingUser);
       userRepository.save(existingUser);
     }
     return null;
