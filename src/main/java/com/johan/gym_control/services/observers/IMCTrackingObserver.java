b58fcd341a8db5d6e5f9bec5c41a0ed6ceaa67db
Johan <johanvilla64@gmail.com>
2025-02-22 22:00:16 -0500

    refactor(observers): :art: reorder import statements in IMCTrackingObserver for consistency

--- src/main/java/com/johan/gym_control/services/observers/IMCTrackingObserver.java
+++ src/main/java/com/johan/gym_control/services/observers/IMCTrackingObserver.java
@@ -1,9 +1,10 @@
 package com.johan.gym_control.services.observers;
 
+import org.springframework.stereotype.Component;
+
 import com.johan.gym_control.models.User;
 import com.johan.gym_control.services.imcTracking.UpdateIMCCommand;
 import com.johan.gym_control.services.observers.interfaces.IMCObserver;
-import org.springframework.stereotype.Component;
 
 @Component
 public class IMCTrackingObserver implements IMCObserver {
