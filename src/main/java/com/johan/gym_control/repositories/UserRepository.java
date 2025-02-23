6e35e93fda9af8a760ea50e1c4ca745d601fe2bf
Johan <johanvilla64@gmail.com>
2025-02-23 18:35:21 -0500

    feat(repositories): :sparkles: add method to find users by isActive status in UserRepository

--- src/main/java/com/johan/gym_control/repositories/UserRepository.java
+++ src/main/java/com/johan/gym_control/repositories/UserRepository.java
@@ -1,13 +1,16 @@
 package com.johan.gym_control.repositories;
 
-import com.johan.gym_control.models.Trainer;
-import com.johan.gym_control.models.User;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.johan.gym_control.models.Trainer;
+import com.johan.gym_control.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByTrainer(Trainer trainer);
+
+    List<User> findByIsActive(Boolean isActive);
 }
