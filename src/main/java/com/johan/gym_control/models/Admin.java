28f8ea20aac2900c53fd88b640c3af8bc1558cd5
Johan <johanvilla64@gmail.com>
2025-03-05 19:44:34 -0500

    refactor(models): :recycle: update Admin class to include constructor with Role assignment and remove unused annotations

--- src/main/java/com/johan/gym_control/models/Admin.java
+++ src/main/java/com/johan/gym_control/models/Admin.java
@@ -1,19 +1,26 @@
 package com.johan.gym_control.models;
 
+import com.johan.gym_control.models.enums.Role;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-// @NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor
+// @AllArgsConstructor
 @Entity
+// @Builder
 @Table(name = "Administradores")
 public class Admin extends Person {
+
+  public Admin(String name, String lastName, String email, String password) {
+    super(name, email, password, Role.ADMIN);
+  }
 
   @PrePersist
   public void prePersist() {
