a9a47b937e8a771996be476cd93482ccfbe2ec65
Johan <johanvilla64@gmail.com>
2025-03-18 16:29:25 -0500

    feat(entrylog): :sparkles: enhance EntryLogRepository with methods for retrieving recent entry logs and ordering by timestamp

--- src/main/java/com/johan/gym_control/repositories/EntryLogRepository.java
+++ src/main/java/com/johan/gym_control/repositories/EntryLogRepository.java
@@ -3,11 +3,22 @@ package com.johan.gym_control.repositories;
 import com.johan.gym_control.models.EntryLog;
 import com.johan.gym_control.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EntryLogRepository extends JpaRepository<EntryLog, Long> {
-    List<EntryLog> findByUser(User user);
+  List<EntryLog> findByUserOrderByTimestampDesc(User user);
+
+  @Query("SELECT e FROM EntryLog e WHERE e.user = :user AND e.timestamp >= :timeLimit ORDER BY e.timestamp DESC")
+  List<EntryLog> findRecentByUser(@Param("user") User user, @Param("timeLimit") Date timeLimit);
+
+  Optional<EntryLog> findFirstByUserOrderByTimestampDesc(User user);
+
+  List<EntryLog> findAllByOrderByTimestampDesc();
 }
