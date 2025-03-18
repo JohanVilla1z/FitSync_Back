c1054a6deba2fff35d6ea41346cdf9d82ab5782b
Johan <johanvilla64@gmail.com>
2025-03-18 16:33:55 -0500

    feat(equipment): :sparkles: add EquipmentBorrowedDTO for managing borrowed equipment details

---
+++ src/main/java/com/johan/gym_control/models/dto/equipment/EquipmentBorrowedDTO.java
@@ -0,0 +1,16 @@
+package com.johan.gym_control.models.dto.equipment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EquipmentBorrowedDTO {
+  private Long id;
+  private String name;
+  private String description;
+}
