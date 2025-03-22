1cc07a359e76e3ae195583033c7b0924bae036c7
Johan <johanvilla64@gmail.com>
2025-03-22 09:55:22 -0500

    feat(application): :sparkles: enable JPA auditing in the application

--- src/main/java/com/johan/gym_control/Application.java
+++ src/main/java/com/johan/gym_control/Application.java
@@ -1,13 +1,15 @@
-package com.johan.GymControl;
+package com.johan.gym_control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
