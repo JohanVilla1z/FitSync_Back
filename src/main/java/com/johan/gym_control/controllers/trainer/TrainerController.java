65abde2c5d268fad2357b15e614290d712e5c105
Johan <johanvilla64@gmail.com>
2025-03-26 13:17:46 -0500

    feat(trainer): :sparkles: add assignUserToTrainer endpoint for user assignment to trainers

--- src/main/java/com/johan/gym_control/controllers/trainer/TrainerController.java
+++ src/main/java/com/johan/gym_control/controllers/trainer/TrainerController.java
@@ -1,8 +1,24 @@
 package com.johan.gym_control.controllers.trainer;
 
-import java.net.URI;
-import java.util.List;
-
+import com.johan.gym_control.models.Trainer;
+import com.johan.gym_control.models.dto.trainer.AssignUserRequest;
+import com.johan.gym_control.models.dto.trainer.TrainerCreateRequest;
+import com.johan.gym_control.models.dto.trainer.TrainerResponseDTO;
+import com.johan.gym_control.services.trainer.AssignUserToTrainerCommand;
+import com.johan.gym_control.services.trainer.CreateTrainerCommand;
+import com.johan.gym_control.services.trainer.DeleteTrainerCommand;
+import com.johan.gym_control.services.trainer.GetAllTrainersCommand;
+import com.johan.gym_control.services.trainer.GetTrainerByIdCommand;
+import com.johan.gym_control.services.trainer.ToggleTrainerActiveStatusCommand;
+import com.johan.gym_control.services.trainer.UpdateTrainerCommand;
+import com.johan.gym_control.utils.TrainerMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,22 +31,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.johan.gym_control.models.Trainer;
-import com.johan.gym_control.models.dto.trainer.TrainerCreateRequest;
-import com.johan.gym_control.services.trainer.CreateTrainerCommand;
-import com.johan.gym_control.services.trainer.DeleteTrainerCommand;
-import com.johan.gym_control.services.trainer.GetAllTrainersCommand;
-import com.johan.gym_control.services.trainer.GetTrainerByIdCommand;
-import com.johan.gym_control.services.trainer.ToggleTrainerActiveStatusCommand;
-import com.johan.gym_control.services.trainer.UpdateTrainerCommand;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/trainer")
@@ -42,27 +45,29 @@ public class TrainerController {
   private final GetTrainerByIdCommand getTrainerByIdCommand;
   private final GetAllTrainersCommand getAllTrainersCommand;
   private final ToggleTrainerActiveStatusCommand toggleTrainerActiveStatusCommand;
+  private final AssignUserToTrainerCommand assignUserToTrainerCommand;
+  private final TrainerMapper trainerMapper;
 
   @Operation(summary = "Crear un nuevo entrenador", description = "Permite crear un nuevo entrenador en el sistema.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "201", description = "Entrenador creado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
-      @ApiResponse(responseCode = "400", description = "Datos inválidos", content = @Content(mediaType = "application/json"))
+          @ApiResponse(responseCode = "201", description = "Entrenador creado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
+          @ApiResponse(responseCode = "400", description = "Datos inválidos", content = @Content(mediaType = "application/json"))
   })
   @PostMapping
   public ResponseEntity<Trainer> createTrainer(@Valid @RequestBody TrainerCreateRequest request) {
     Trainer createdTrainer = createTrainerCommand.execute(request);
     URI location = ServletUriComponentsBuilder
-        .fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(createdTrainer.getId())
-        .toUri();
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(createdTrainer.getId())
+            .toUri();
     return ResponseEntity.created(location).body(createdTrainer);
   }
 
   @Operation(summary = "Actualizar un entrenador", description = "Permite actualizar los datos de un entrenador existente.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Entrenador actualizado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
-      @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
+          @ApiResponse(responseCode = "200", description = "Entrenador actualizado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
+          @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
   })
   @PutMapping("/{id}")
   public ResponseEntity<Trainer> updateTrainer(@PathVariable Long id, @RequestBody Trainer trainer) {
@@ -73,8 +78,8 @@ public class TrainerController {
 
   @Operation(summary = "Eliminar un entrenador", description = "Permite eliminar un entrenador del sistema.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "204", description = "Entrenador eliminado exitosamente"),
-      @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
+          @ApiResponse(responseCode = "204", description = "Entrenador eliminado exitosamente"),
+          @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
   })
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteTrainer(@PathVariable Long id) {
@@ -84,35 +89,48 @@ public class TrainerController {
 
   @Operation(summary = "Obtener un entrenador por su ID", description = "Devuelve los detalles de un entrenador específico.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Entrenador encontrado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
-      @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
+          @ApiResponse(responseCode = "200", description = "Entrenador encontrado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
+          @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
   })
   @GetMapping("/{id}")
-  public ResponseEntity<Trainer> getTrainerById(@PathVariable Long id) {
+  public ResponseEntity<TrainerResponseDTO> getTrainerById(@PathVariable Long id) {
     return getTrainerByIdCommand.execute(id)
-        .map(ResponseEntity::ok)
-        .orElse(ResponseEntity.notFound().build());
+            .map(trainer -> ResponseEntity.ok(trainerMapper.convertToDTO(trainer)))
+            .orElse(ResponseEntity.notFound().build());
   }
 
-  @Operation(summary = "Obtener todos los entrenadores", description = "Devuelve una lista de todos los entrenadores registrados.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Lista de entrenadores obtenida exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class)))
-  })
   @GetMapping
-  public ResponseEntity<List<Trainer>> getAllTrainers() {
+  public ResponseEntity<List<TrainerResponseDTO>> getAllTrainers() {
     List<Trainer> trainers = getAllTrainersCommand.execute();
-    return ResponseEntity.ok(trainers);
+    List<TrainerResponseDTO> dtos = trainers.stream()
+            .map(trainerMapper::convertToDTO)
+            .collect(Collectors.toList());
+    return ResponseEntity.ok(dtos);
   }
 
   @Operation(summary = "Cambiar el estado activo de un entrenador", description = "Permite alternar el estado activo de un entrenador.")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Estado del entrenador cambiado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
-      @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
+          @ApiResponse(responseCode = "200", description = "Estado del entrenador cambiado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Trainer.class))),
+          @ApiResponse(responseCode = "404", description = "Entrenador no encontrado", content = @Content(mediaType = "application/json"))
   })
   @PutMapping("/{id}/toggle-status")
   @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Trainer> toggleTrainerStatus(@PathVariable Long id) {
     Trainer updatedTrainer = toggleTrainerActiveStatusCommand.execute(id);
     return ResponseEntity.ok(updatedTrainer);
+  }
+
+  @Operation(summary = "Asignar un usuario a un entrenador", description = "Asigna un usuario a la lista de usuarios de un entrenador.")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Usuario asignado exitosamente"),
+          @ApiResponse(responseCode = "400", description = "Datos inválidos", content = @Content(mediaType = "application/json")),
+          @ApiResponse(responseCode = "404", description = "Usuario o entrenador no encontrado", content = @Content(mediaType = "application/json"))
+  })
+  @PostMapping("/assign-user")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<Void> assignUserToTrainer(@Valid @RequestBody AssignUserRequest request) {
+    Long[] params = {request.getUserId(), request.getTrainerId()};
+    assignUserToTrainerCommand.execute(params);
+    return ResponseEntity.ok().build();
   }
 }
