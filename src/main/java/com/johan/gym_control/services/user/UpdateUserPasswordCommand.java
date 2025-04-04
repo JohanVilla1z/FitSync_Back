7ecc085c5c4118ec6f0a49b1cbcd238f1b2078a3
Johan <johanvilla64@gmail.com>
2025-04-03 20:36:50 -0500

    refactor(service): :recycle: update UpdateUserPasswordCommand to use new UpdatePasswordRequest DTO and improve code organization

--- src/main/java/com/johan/gym_control/services/user/UpdateUserPasswordCommand.java
+++ src/main/java/com/johan/gym_control/services/user/UpdateUserPasswordCommand.java
@@ -1,11 +1,13 @@
 package com.johan.gym_control.services.user;
 
-import com.johan.gym_control.models.User;
-import com.johan.gym_control.models.dto.user.UpdatePasswordRequest;
-import com.johan.gym_control.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.johan.gym_control.models.User;
+import com.johan.gym_control.models.dto.auth.UpdatePasswordRequest;
+import com.johan.gym_control.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
@@ -15,7 +17,7 @@ public class UpdateUserPasswordCommand {
 
   public void execute(String userEmail, UpdatePasswordRequest request) {
     User user = userRepository.findByEmail(userEmail)
-            .orElseThrow(() -> new IllegalArgumentException("User not found with email: " + userEmail));
+        .orElseThrow(() -> new IllegalArgumentException("User not found with email: " + userEmail));
 
     if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
       throw new IllegalArgumentException("Current password is incorrect");
