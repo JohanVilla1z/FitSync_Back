5339166d3e139d7cc87aa7d682e4576298a1bebf
Johan <johanvilla64@gmail.com>
2025-04-19 12:53:52 -0500

    fix(security): remove unused Autowired import from SecurityConfig

--- src/main/java/com/johan/gym_control/config/security/SecurityConfig.java
+++ src/main/java/com/johan/gym_control/config/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.johan.gym_control.config.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
