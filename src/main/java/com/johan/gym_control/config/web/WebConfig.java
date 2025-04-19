958f2735f66c609b8954e4c66143ddce047b8bc8
Johan <johanvilla64@gmail.com>
2025-04-19 13:10:31 -0500

    fix(cors): :wrench: implement CORS configuration in WebConfig to allow requests from specified origins

--- src/main/java/com/johan/gym_control/config/web/WebConfig.java
+++ src/main/java/com/johan/gym_control/config/web/WebConfig.java
@@ -1,9 +1,20 @@
 package com.johan.gym_control.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("https://fitsyncapp.netlify.app")
+      .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+      .allowedHeaders("*")
+      .allowCredentials(true);
+  }
 
   // CorsFilter eliminado para desactivar protección CORS personalizada
 
