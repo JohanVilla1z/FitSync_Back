08f31afb8f1fcc37545f179b6f98865fc1228551
Johan <johanvilla64@gmail.com>
2025-04-20 13:16:02 -0500

    fix(trainer): :art: reformat code for improved readability in TrainerMapper

--- src/main/java/com/johan/gym_control/utils/TrainerMapper.java
+++ src/main/java/com/johan/gym_control/utils/TrainerMapper.java
@@ -1,23 +1,24 @@
 package com.johan.gym_control.utils;
 
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
 import com.johan.gym_control.models.Trainer;
 import com.johan.gym_control.models.User;
 import com.johan.gym_control.models.dto.trainer.TrainerResponseDTO;
-import org.springframework.stereotype.Component;
-
-import java.util.stream.Collectors;
 
 @Component
 public class TrainerMapper {
 
   public TrainerResponseDTO convertToDTO(Trainer trainer) {
     return TrainerResponseDTO.builder()
-            .id(trainer.getId())
-            .name(trainer.getName())
-            .email(trainer.getEmail())
-            .active(trainer.getIsActive())
-            .available(trainer.isTrainerAvailable())
-            .userIds(trainer.getUsers().stream().map(User::getId).collect(Collectors.toList()))
-            .build();
+        .id(trainer.getId())
+        .name(trainer.getName())
+        .email(trainer.getEmail())
+        .active(trainer.getIsActive())
+        .available(trainer.isTrainerAvailable())
+        .userIds(trainer.getUsers().stream().map(User::getId).collect(Collectors.toList()))
+        .build();
   }
 }
