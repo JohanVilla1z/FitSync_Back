0066974a27bde4c3913f3acd84b0e3b865b856cf
Johan <johanvilla64@gmail.com>
2025-04-21 17:56:51 -0500

    refactor(equipment): :fire: remove unused import statements for cleaner code

--- src/main/java/com/johan/gym_control/models/Equipment.java
+++ src/main/java/com/johan/gym_control/models/Equipment.java
@@ -9,8 +9,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
