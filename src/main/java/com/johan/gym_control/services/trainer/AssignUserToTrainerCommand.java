885fdc89aed5817a53ffef51b54b12dde3c6b14c
Johan <johanvilla64@gmail.com>
2025-04-29 15:14:01 -0500

    refactor(AssignUserToTrainerCommand): :art: reorder import statements and adjust code formatting for consistency

--- src/main/java/com/johan/gym_control/services/trainer/AssignUserToTrainerCommand.java
+++ src/main/java/com/johan/gym_control/services/trainer/AssignUserToTrainerCommand.java
@@ -1,11 +1,12 @@
 package com.johan.gym_control.services.trainer;
 
+import org.springframework.stereotype.Service;
+
 import com.johan.gym_control.models.Trainer;
 import com.johan.gym_control.models.User;
 import com.johan.gym_control.repositories.TrainerRepository;
 import com.johan.gym_control.repositories.UserRepository;
 import com.johan.gym_control.services.interfaces.ICommandParametrized;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AssignUserToTrainerCommand implements ICommandParametrized<Void, Long[]> {
@@ -23,9 +24,9 @@ public class AssignUserToTrainerCommand implements ICommandParametrized<Void, Lo
     Long trainerId = params[1];
 
     User user = userRepository.findById(userId)
-            .orElseThrow(() -> new IllegalArgumentException("User with ID " + userId + " does not exist."));
+        .orElseThrow(() -> new IllegalArgumentException("User with ID " + userId + " does not exist."));
     Trainer trainer = trainerRepository.findByIdWithUsers(trainerId)
-            .orElseThrow(() -> new IllegalArgumentException("Trainer with ID " + trainerId + " does not exist."));
+        .orElseThrow(() -> new IllegalArgumentException("Trainer with ID " + trainerId + " does not exist."));
 
     if (!trainer.isTrainerAvailable()) {
       throw new IllegalStateException("Trainer with ID " + trainerId + " is not available.");
